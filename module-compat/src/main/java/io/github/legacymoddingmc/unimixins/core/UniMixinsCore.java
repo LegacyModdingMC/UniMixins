a7a28f42142639e97eb56b49acaf42795f27772d
makamys <makamys@outlook.com>
2023-02-03 19:59:39 +0100

    Fix ASMRemapperTransformer not getting registered in time

--- module-compat/src/main/java/io/github/legacymoddingmc/unimixins/core/UniMixinsCore.java
+++ module-compat/src/main/java/io/github/legacymoddingmc/unimixins/core/UniMixinsCore.java
@@ -9,19 +9,21 @@ import java.util.Map;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin.MCVersion;
 import io.github.legacymoddingmc.unimixins.core.asm.ASMRemapperTransformer;
+import net.minecraft.launchwrapper.Launch;
 
 @MCVersion("1.7.10")
 public class UniMixinsCore implements IFMLLoadingPlugin {
 
     public UniMixinsCore() {
         LOGGER.info("Instantiating CoreTweaksPlugin");
+
+        // We register the transformer this way to register it as early as possible.
+        Launch.classLoader.registerTransformer(relativeClassName("asm.ASMRemapperTransformer"));
     }
 
     @Override
     public String[] getASMTransformerClass() {
-        List<String> transformerClasses = new ArrayList<>();
-        transformerClasses.add(ASMRemapperTransformer.class.getName());
-        return transformerClasses.toArray(new String[] {});
+        return new String[] {};
     }
 
     private static String relativeClassName(String relName) {
