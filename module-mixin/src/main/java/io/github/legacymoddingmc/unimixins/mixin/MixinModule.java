1af2e989df6abbda399e43f0e2146d87312f0f7d
makamys <makamys@outlook.com>
2023-02-03 11:38:50 +0100

    Add @Mod annotation to Mixin module

---
+++ module-mixin/src/main/java/io/github/legacymoddingmc/unimixins/mixin/MixinModule.java
@@ -0,0 +1,10 @@
+package io.github.legacymoddingmc.unimixins.mixin;
+
+import cpw.mods.fml.common.Mod;
+
+/**
+ * An empty mod class provided to make it impossible to load multiple editions
+ * of the module at the same time.
+ */
+@Mod(modid = "unimixins-mixin", version = "@VERSION@")
+public class MixinModule {}
