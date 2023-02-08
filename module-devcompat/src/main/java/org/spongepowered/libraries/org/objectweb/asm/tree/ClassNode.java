4c8eb4ad4c0dbb4078bde4860cae8ea7eba32180
makamys <makamys@outlook.com>
2023-02-08 22:18:01 +0100

    Remove ClassNode dummy from devcompat
    I realized adding compat for this is unnecessary, if someone wants to migrate a
    mod to UniMixins, requiring them to change a single line in their mixin config
    plugin is not asking much.

--- module-devcompat/src/main/java/org/spongepowered/libraries/org/objectweb/asm/tree/ClassNode.java
+++
@@ -1,5 +0,0 @@
-package org.spongepowered.libraries.org.objectweb.asm.tree;
-
-/** Dummy class to fix compilation errors in mods that use the MBL-style shaded ASM package name. */
-public class ClassNode {
-}
