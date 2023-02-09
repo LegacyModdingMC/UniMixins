0edc021c4697c50c0a02ba04284a8a6084058c8c
makamys <makamys@outlook.com>
2023-02-09 14:11:31 +0100

    Remove LWJGL3ify compat hack as it's no longer needed

--- module-compat/src/main/java/io/github/legacymoddingmc/unimixins/compat/CompatCore.java
+++ module-compat/src/main/java/io/github/legacymoddingmc/unimixins/compat/CompatCore.java
@@ -1,13 +1,10 @@
 package io.github.legacymoddingmc.unimixins.compat;
 
-import java.lang.reflect.Field;
 import java.util.Map;
-import java.util.Set;
 
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin.MCVersion;
 import net.minecraft.launchwrapper.Launch;
-import net.minecraft.launchwrapper.LaunchClassLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -25,28 +22,9 @@ public class CompatCore implements IFMLLoadingPlugin {
 
     @Override
     public String[] getASMTransformerClass() {
-        applyLWJGL3ifyCompatibilityHack();
         return new String[] {};
     }
 
-    /** LWJGL3ify excludes transformation of its core package, which prevents us from remapping its
-     * LwjglRedirectTransformer. However, the exclusion appears to be unnecessary, since its transformers already
-     * have checks that prevent ClassCircularityErrors.
-     */
-    private static void applyLWJGL3ifyCompatibilityHack() {
-        try {
-            Field transformerExceptionsF = LaunchClassLoader.class.getDeclaredField("transformerExceptions");
-            transformerExceptionsF.setAccessible(true);
-            Set<String> transformerExceptions = (Set<String>)transformerExceptionsF.get(Launch.classLoader);
-            final String lwjgl3ifyCorePackage = "me.eigenraven.lwjgl3ify.core";
-            if(transformerExceptions.contains(lwjgl3ifyCorePackage)) {
-                LOGGER.info("Removing '" + lwjgl3ifyCorePackage + "' transformer exclusion to apply LWJGL3ify compatibility hack.");
-                transformerExceptions.remove(lwjgl3ifyCorePackage);
-            }
-        } catch(Exception e){
-            LOGGER.debug("Encountered exception while trying to apply LWJGL3ify compatibility hack: " + e);
-        }
-    }
     private static String relativeClassName(String relName) {
         String name = CompatCore.class.getName();
         name = name.substring(0, name.lastIndexOf('.') + 1);
