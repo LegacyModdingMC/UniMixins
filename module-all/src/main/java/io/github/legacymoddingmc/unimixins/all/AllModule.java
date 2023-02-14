33d4190e1ab97f7f79a5277bc7db4f6a61ba0a90
makamys <makamys@outlook.com>
2023-02-14 05:33:32 +0100

    Apparently you need a @Mod to have a mod in the mod list GUI

---
+++ module-all/src/main/java/io/github/legacymoddingmc/unimixins/all/AllModule.java
@@ -0,0 +1,10 @@
+package io.github.legacymoddingmc.unimixins.all;
+
+import cpw.mods.fml.common.Mod;
+
+/**
+ * This is only here so unimixins shows up in the mod list GUI as a single mod.
+ */
+@Mod(modid = "unimixins", version = "@VERSION@")
+public class AllModule {
+}
