ee3ac092d3c39badb25f25770c45d3c4ed2653f6
makamys <makamys@outlook.com>
2023-02-17 01:33:48 +0100

    Update comment of enhanceCrashReports

--- module-compat/src/main/java/io/github/legacymoddingmc/unimixins/compat/CompatConfig.java
+++ module-compat/src/main/java/io/github/legacymoddingmc/unimixins/compat/CompatConfig.java
@@ -5,6 +5,6 @@ import io.github.legacymoddingmc.unimixins.common.config.AnnotatedProperties;
 public class CompatConfig {
     @AnnotatedProperties.ConfigString(def = "true", com = "Remap references to Mixin's shaded ASM.", cat = "compat")
     public static boolean enableRemapper;
-    @AnnotatedProperties.ConfigString(def = "true", com = "Include Mixin errors in crash reports.", cat = "compat")
+    @AnnotatedProperties.ConfigString(def = "true", com = "Include mixin errors and list of applied mixins in crash reports.", cat = "compat")
     public static boolean enhanceCrashReports;
 }
