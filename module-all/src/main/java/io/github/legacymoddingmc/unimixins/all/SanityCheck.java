e1b148d5c75de931b86818cffab30cbe59aab9b9
makamys <makamys@outlook.com>
2023-03-23 15:28:58 +0100

    Add warning if Mixin is getting bootstrapped from a foreign jar

--- module-all/src/main/java/io/github/legacymoddingmc/unimixins/all/SanityCheck.java
+++ module-all/src/main/java/io/github/legacymoddingmc/unimixins/all/SanityCheck.java
@@ -27,10 +27,8 @@ public class SanityCheck {
     static void doSanityCheck() {
         List<String> warnings = new ArrayList<>();
 
-        List<String> concerningJars = getConcerningJars();
-        if(!concerningJars.isEmpty()) {
-            warnings.add("Detected incompatible jars: " + concerningJars);
-        }
+        checkConcerningJars(warnings);
+        checkMixinContainer(warnings);
 
         if(!warnings.isEmpty()) {
             // Any throwables we throw here will get caught, so all we can do is warn.
@@ -43,6 +41,47 @@ public class SanityCheck {
             LOGGER.error("The game will almost certainly crash!");
             LOGGER.fatal("======================================================================================");
             throw new Error(String.join("; ", warnings)); // Attention grabbing stack trace
+        }
+    }
+
+    private static void checkMixinContainer(List<String> warnings) {
+        String mixinCoreJarName = getJarName(SanityCheck.class.getResource("/io/github/legacymoddingmc/unimixins/mixin/MixinCore.class"));
+        String mixinTweakerJarName = getJarName(SanityCheck.class.getResource("/org/spongepowered/asm/launch/MixinTweaker.class"));
+
+        LOGGER.debug("Name of jar containing MixinCore: " + mixinCoreJarName);
+        LOGGER.debug("Name of jar containing MixinTweaker: " + mixinTweakerJarName);
+
+        if(!mixinCoreJarName.equals(mixinTweakerJarName)) {
+            boolean isSuspectedToBeDueToNaming =
+                    mixinCoreJarName != null && mixinTweakerJarName != null
+                        && mixinCoreJarName.compareToIgnoreCase(mixinTweakerJarName) > 0;
+            warnings.add("A different version of Mixin (the one inside " + stringOr(mixinTweakerJarName, "unknown") + ") is getting loaded instead of UniMixins's one (inside " + stringOr(mixinCoreJarName, "unknown") + ")!" + (isSuspectedToBeDueToNaming ? " This is probably because because UniMixins's jar name comes later alphabetically. Try renaming it to come first (for example, by adding a '!' character at the beginning of the file name.)" : ""));
+        }
+    }
+
+    private static String stringOr(String s, String alternative) {
+        return s == null ? alternative : s;
+    }
+
+    private static String getJarName(URL url) {
+        if(url != null) {
+            String urlStr = url.toString();
+            if (urlStr.contains("!/")) {
+                int exclamationIdx = urlStr.indexOf("!/");
+                String preExclamationSubstring = urlStr.substring(0, exclamationIdx);
+                int lastSlash = preExclamationSubstring.lastIndexOf('/');
+                if (lastSlash != -1) {
+                    return preExclamationSubstring.substring(lastSlash + 1);
+                }
+            }
+        }
+        return null;
+    }
+
+    private static void checkConcerningJars(List<String> warnings) {
+        List<String> concerningJars = getConcerningJars();
+        if(!concerningJars.isEmpty()) {
+            warnings.add("Detected incompatible jars: " + concerningJars);
         }
     }
 
