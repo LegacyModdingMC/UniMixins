8e3e7e49e51ca876ae4ac02ea5f37a0f7847726f
makamys <makamys@outlook.com>
2023-10-17 04:40:47 +0200

    Make compat module use new Mixingasm API
    (But keep using the old one as a fallback.)

--- module-compat/src/main/java/io/github/legacymoddingmc/unimixins/compat/asm/LegacyGTNHMixinExtrasGenerator.java
+++ module-compat/src/main/java/io/github/legacymoddingmc/unimixins/compat/asm/LegacyGTNHMixinExtrasGenerator.java
@@ -2,15 +2,19 @@ package io.github.legacymoddingmc.unimixins.compat.asm;
 
 import static io.github.legacymoddingmc.unimixins.compat.CompatCore.LOGGER;
 
+import makamys.mixingasm.api.MixinSafeTransformer;
 import makamys.mixingasm.api.TransformerInclusions;
 import net.minecraft.launchwrapper.IClassTransformer;
 import net.minecraft.launchwrapper.Launch;
 
 import java.io.IOException;
 
+@MixinSafeTransformer
 public class LegacyGTNHMixinExtrasGenerator implements IClassTransformer {
     static {
-        if(LegacyGTNHMixinExtrasGenerator.class.getResource("/makamys/mixingasm/api/TransformerInclusions.class") != null) {
+        if(LegacyGTNHMixinExtrasGenerator.class.getResource("/makamys/mixingasm/api/TransformerInclusions.class") != null
+        && LegacyGTNHMixinExtrasGenerator.class.getResource("/makamys/mixingasm/api/MixinSafeTransformer.class") == null) {
+            // Mixingasm < 0.3 compat
             TransformerInclusions.getTransformerInclusionList().add("io.github.legacymoddingmc.unimixins.compat.asm.LegacyGTNHMixinExtrasGenerator");
         }
     }
