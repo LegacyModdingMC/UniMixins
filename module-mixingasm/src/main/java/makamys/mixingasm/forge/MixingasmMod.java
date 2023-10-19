2409b246b3b86cf4545cb448525db87d03fa1873
makamys <makamys@outlook.com>
2023-10-19 06:34:39 +0200

    Mixingasm seems to work now

--- module-mixingasm/src/main/java/makamys/mixingasm/forge/MixingasmMod.java
+++ module-mixingasm/src/main/java/makamys/mixingasm/forge/MixingasmMod.java
@@ -1,10 +1,11 @@
 package makamys.mixingasm.forge;
 
 import cpw.mods.fml.common.Mod;
+import io.github.legacymoddingmc.unimixins.mixingasm.Tags;
 import makamys.mixingasm.Mixingasm;
 
-@Mod(modid = Mixingasm.MODID, version = "@VERSION@", acceptableRemoteVersions = "*")
-@net.minecraftforge.fml.common.Mod(modid = Mixingasm.MODID, version = "@VERSION@", acceptableRemoteVersions = "*")
+@Mod(modid = Mixingasm.MODID, version = Tags.VERSION, acceptableRemoteVersions = "*")
+@net.minecraftforge.fml.common.Mod(modid = Mixingasm.MODID, version = Tags.VERSION, acceptableRemoteVersions = "*")
 public class MixingasmMod {
 
 }
