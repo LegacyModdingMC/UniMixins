b84465284e63774c9de4303fe3ed39185118c2af
makamys <makamys@outlook.com>
2023-12-12 02:01:03 +0100

    Fill out placeholder stuff

--- src/main/java/io/github/legacymoddingmc/unimixins/example/ExampleMod.java
+++ src/main/java/io/github/legacymoddingmc/unimixins/example/ExampleMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package io.github.legacymoddingmc.unimixins.example;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
@@ -8,7 +8,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "unimixinsexample";
     public static final String VERSION = "1.0";
     
     @EventHandler
