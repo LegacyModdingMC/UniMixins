bd9dca59e233152495f2b98844634ff52aa0b5ac
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-06-07 15:20:17 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@
 // First, we need the mixin module, which means we need Unimixins!
 plugins {
     id("java")
-    id("xyz.wagyourtail.unimined") version "1.3.14"
+    id("xyz.wagyourtail.unimined") version "1.3.14" apply false
 }
 
 // Mod properties
@@ -19,7 +19,11 @@ val forge_version = "10.13.4.1614-1.7.10"
 
 val project_url = "https://github.com/LegacyModdingMC/UniMixins"
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+allprojects {
+    apply { plugin("java") }
+
+    java {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
 }
