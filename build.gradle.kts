685ae7416ff2fe5cf7c78f2db7e83cd9064b2cf2
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-06-07 02:28:59 -0400

    Actually add Minecraft

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,18 @@ plugins {
     id("xyz.wagyourtail.unimined") version "1.3.14"
 }
 
+repositories {
+    maven {
+        name = "wagyourtail releases"
+        url = uri("https://maven.wagyourtail.xyz/releases")
+    }
+    maven {
+        name = "sponge"
+        url = uri("https://repo.spongepowered.org/maven")
+    }
+    mavenCentral()
+}
+
 group = rootProject.properties["group"].toString()
 
 java {
@@ -17,4 +29,17 @@ java {
 lateinit var moduleMixin: SourceSet
 sourceSets {
     moduleMixin = create("module-mixin")
+}
+
+unimined.minecraft(moduleMixin) {
+    version = project.properties["minecraft_version"].toString()
+
+    mappings {
+        searge()
+        mcp("stable", "12-1.7.10")
+    }
+
+    minecraftForge {
+        loader(project.properties["forge_version"].toString())
+    }
 }
