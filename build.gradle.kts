b3a54c73e43be566c55ea28365c0e5976ad9971c
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-06-07 01:56:19 -0400

    Add mavens for plugins

---
+++ build.gradle.kts
@@ -0,0 +1,4 @@
+// The previous buildscript was spaghetti
+// Time will tell if I do better.
+
+// First, we need the mixin module, which means we need Unimixins!
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,31 @@
+pluginManagement {
+    repositories {
+        mavenCentral()
+        maven {
+            url = uri("https://maven.neoforged.net/releases")
+        }
+        maven {
+            url = uri("https://maven.minecraftforge.net/")
+        }
+        maven {
+            url = uri("https://maven.fabricmc.net/")
+        }
+        maven {
+            url = uri("https://maven.wagyourtail.xyz/releases")
+        }
+        maven {
+            url = uri("https://maven.wagyourtail.xyz/snapshots")
+        }
+        gradlePluginPortal {
+            content {
+                excludeGroup("org.apache.logging.log4j")
+            }
+        }
+    }
+}
+
+rootProject.name = "UniMixins"
+
 // include("module-mixin")
 // include("module-compat")
 // include("module-compatfuture")
