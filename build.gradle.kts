33277033d1d900a60c35fc295bf1e4015885b984
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-06-09 01:26:14 -0400

    Fix versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,12 +7,11 @@ import xyz.wagyourtail.unimined.api.unimined
 plugins {
     id("java")
     id("xyz.wagyourtail.unimined") version "1.3.14" apply false
+    id("com.palantir.git-version") version "3.3.0" apply false
 }
 
 // Mod properties
 val modid = "unimixins"
-val archives_base = "+unimixins"
-// http://maven.apache.org/guides/mini/guide-naming-conventions.html
 group = "io.github.legacymoddingmc"
 
 // Forge properties
@@ -22,6 +21,7 @@ val forge_version = "10.13.4.1614-1.7.10"
 subprojects {
     apply(plugin = "java")
     apply(plugin = "xyz.wagyourtail.unimined")
+    apply(plugin = "com.palantir.git-version")
 
     java {
         sourceCompatibility = JavaVersion.VERSION_1_8
@@ -33,6 +33,25 @@ subprojects {
         compileOnly("net.minecraftforge:forge:1.12.2-14.23.5.2860:universal")
     }
 
+    fun getVersion(): String {
+        val override = System.getenv("VERSION")
+        if (override != null) {
+            print("VERSION set! Overriding version to $override")
+            return override
+        }
+
+        val versionDetails: groovy.lang.Closure<com.palantir.gradle.gitversion.VersionDetails> by extra
+        // If it exists, grab the last tag. Otherwise it is UNKNOWN
+        val details = versionDetails()
+        var ret = details.lastTag
+        if (details.commitDistance > 0) ret += "-${details.commitDistance}-${details.gitHash}"
+        if (!details.isCleanTag) ret += "-dirty"
+
+        return ret
+    }
+
+    version = getVersion()
+    base.archivesName = "+unimixins-${ if (project.name == "all") "" else project.name }-1.7.10"
     ext.set("project_url", "https://github.com/LegacyModdingMC/UniMixins")
 
     unimined.minecraft(sourceSets.main.get(), true) {
