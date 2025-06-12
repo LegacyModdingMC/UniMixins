d5b9033c4d4a19d0023934082d03a53c70d82c0c
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-06-12 12:05:20 -0400

    Shade everything in the -all module

--- all/build.gradle.kts
+++ all/build.gradle.kts
@@ -1,3 +1,29 @@
+plugins {
+    id("com.gradleup.shadow") version "8.3.6"
+}
+
+dependencies {
+    implementation(project(":mixin", "shadowArtifact"))
+    implementation(project(":compat", "shadowArtifact"))
+    implementation(project(":mixingasm", "shadowArtifact"))
+    implementation(project(":spongemixins", "shadowArtifact"))
+    implementation(project(":mixinbooterlegacy", "shadowArtifact"))
+    implementation(project(":gasstation", "shadowArtifact"))
+    implementation(project(":gtnhmixins", "shadowArtifact"))
+    implementation(project(":mixinextras", "shadowArtifact"))
+}
+
+tasks.shadowJar {
+    archiveClassifier = ""
+
+    // Exclude errant license files
+    exclude("LICENSE*")
+}
+
+tasks.jar {
+    dependsOn(tasks.shadowJar)
+    enabled = false
+}
 
 unimined.minecraft {
 
