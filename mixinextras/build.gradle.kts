07ca8dbe0faf5f59595547a94d2fca24f967158f
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-06-12 10:27:00 -0400

    Add MixinExtras

---
+++ mixinextras/build.gradle.kts
@@ -0,0 +1,60 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.apache.tools.ant.filters.ReplaceTokens
+
+plugins {
+	id("com.gradleup.shadow") version "8.3.6"
+}
+
+val mixinExtrasVersion = "0.4.1"
+
+val shadowSources by configurations.creating
+
+dependencies {
+	shadow("io.github.llamalad7:mixinextras-common:$mixinExtrasVersion")
+	shadowSources("io.github.llamalad7:mixinextras-common:$mixinExtrasVersion:sources")
+}
+version = "$version+$mixinExtrasVersion"
+
+tasks.shadowJar {
+	archiveClassifier = ""
+	configurations = listOf(project.configurations.shadow.get())
+	
+	relocate("org.objectweb.asm", "org.spongepowered.asm.lib")
+	
+	manifest {
+		attributes (
+			"TweakClass" to "org.spongepowered.asm.launch.MixinTweaker",
+			"FMLCorePluginContainsFMLMod" to "true",
+			"ForceLoadAsMod" to "true",
+			"FMLCorePlugin" to "io.github.legacymoddingmc.unimixins.mixinextras.MixinExtrasCore",
+		)
+	}
+}
+
+val shadowSourcesJar by tasks.registering(ShadowJar::class) {
+	from(sourceSets.main.get().allSource)
+	archiveClassifier = "sources"
+	configurations = listOf(shadowSources)
+}
+
+//tasks.named("sourcesJar") {
+//	enabled = false
+//	dependsOn(shadowSourcesJar)
+//}
+
+tasks.jar {
+	enabled = false
+	dependsOn(tasks.shadowJar)
+}
+
+tasks.processResources {
+	files("mcmod.info") {
+		filter<ReplaceTokens>("tokens" to mapOf(
+			"mixinExtrasVersion" to mixinExtrasVersion
+		))
+	}
+}
+
+unimined.minecraft {
+
+}
