5c9214387845f150ca808b09d137d5720fbdf4d0
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-06-13 16:35:43 -0400

    Move subprojects block into buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,9 +5,15 @@ plugins {
 
 repositories {
     gradlePluginPortal()
+
+    maven {
+        url = uri("https://maven.wagyourtail.xyz/releases")
+    }
 }
 
 dependencies {
+    implementation("xyz.wagyourtail.unimined:xyz.wagyourtail.unimined.gradle.plugin:1.3.14")
+    implementation("com.palantir.git-version:com.palantir.git-version.gradle.plugin:3.3.0")
     implementation("com.gradleup.shadow:com.gradleup.shadow.gradle.plugin:8.3.6")
     implementation("com.google.code.gson:gson:2.13.1")
     implementation("org.apache.ant:ant:1.10.15")
